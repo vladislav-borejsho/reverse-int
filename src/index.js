942bf01bbca64da39d46b5d5620d0c8f6b67d904
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-07-28 19:56:02 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const str = n.toString();
+    const arr = [];
+    for (let i=0; i<str.length; i++) {
+        if (str[i] !== '-') {
+            arr.push(str[i]);
+    }}        
+    const rev = arr.reverse();
+    const res = rev.map(Number);
+    const newStr = res.join('');
+        return Number(newStr);
 }
+
